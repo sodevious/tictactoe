8b6b542de739abf9980f4e7f304526d2b7b0aee3
Nicole Dominguez <sodevious.net@gmail.com>
2013-06-02 22:56:51 -0400

    tryina figure this out

---
+++ js/application.js
@@ -0,0 +1,18 @@
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 10;
+var five = 20;
+var six = 30;
+var seven = 100;
+var eight = 200;
+var nine = 300;
+
+function add() {
+
+}
+if 
+ 
+alert(one+nine); //Will give us 1!
+
